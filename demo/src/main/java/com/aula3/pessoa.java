3e5de95c0e46e5a03e4d96efe3088d9af64deef7
mur1love <murilormleal@gmail.com>
2025-10-09 19:27:24 -0300

    deixando a classe pessoa piorada

--- demo/src/main/java/com/aula3/pessoa.java
+++ demo/src/main/java/com/aula3/pessoa.java
@@ -1,31 +1,26 @@
 package com.aula3;
-//Classe que não segue boas pŕaticas de programação orientada a objetos
-
-//Nome da classe em minúsculo (pessoa)
-//Atributos públicos (quebra de encapsulamento)
-//Métodos e variáveis com nomes inconsistentes (Nome, cpf, setTudo)
-//Método setTudo faz várias tarefas (baixa coesão)
-//Falta de construtor sobrecarregado e uso do this()
-//Ausência de comentários e formatação inconsistente
-
+//Classe que não segue boas práticas de programação orientada a objetos
 public class pessoa {
-    public String Nome;
-    public String cpf;
-    public int idade;
-    
-    public pessoa(String n, String c, int i){
-        Nome = n;
-        cpf = c;
-        idade = i;
-    }
 
-    public void setTudo(String n, String c, int i){
-        Nome = n;
-        cpf = c;
-        idade = i;
-    }
+   public String NOMEpessoa;
+   public String cpf_do_Usuário;
+   public int idadêDaPessoa;
 
-    public void mostrar(){
-        System.out.println("Nome: " + Nome + " Cpf: " + cpf + " Idade: " + idade);
-    }
+   public pessoa(String NomePessoa, String CPF, int IDADEpessoaÇ){
+       NOMEpessoa = NomePessoa;
+       cpf_do_Usuário = CPF;
+       idadêDaPessoa = IDADEpessoaÇ;
+   }
+
+   public void settudooo(String NOVOnome, String novocpf, int Idadê){
+       NOMEpessoa = NOVOnome;
+       cpf_do_Usuário = novocpf;
+       idadêDaPessoa = Idadê;
+   }
+
+   public void mostrár_dádos(){
+       System.out.println("Nome da PESSOA: " + NOMEpessoa +
+                          " | CPF: " + cpf_do_Usuário +
+                          " | Idadê: " + idadêDaPessoa);
+   }
 }
